d5a3c6b1e3c017cb50b887504ca871f6d67dba84
Dinh Viet Long <long37k1999@gmail.com>
2024-06-17 11:28:55 +0700

    tao chuc nang signIn and signUp va conect Firebase

---
+++ app/src/main/java/com/example/chatapp/activities/SignInActivity.java
@@ -0,0 +1,115 @@
+package com.example.chatapp.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.chatapp.R;
+import com.example.chatapp.databinding.ActivitySignInBinding;
+import com.example.chatapp.utilities.Constants;
+import com.example.chatapp.utilities.PreferenceManager;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+
+public class SignInActivity extends AppCompatActivity {
+    private ActivitySignInBinding binding;
+    private PreferenceManager preferenceManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        // xu ly khi da dang ngap tai khoan
+//        if (preferenceManager.getBoolean(Constants.KEY_IS_SIGNED_IN)){
+//            Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+//            startActivity(intent);
+//            finish();
+//        }
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        EdgeToEdge.enable(this);
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        setListeners();
+    }
+
+    private void setListeners(){
+        binding.txtCreateAccount.setOnClickListener(v ->
+                startActivity(new Intent(getApplicationContext(),SignUpActivity.class)));
+
+        binding.buttonSignIn.setOnClickListener(v->{
+            if (isValidSignInDetails()){
+                signIn();
+            }
+        });
+    }
+
+//    xư lý dang nhap
+    private void signIn(){
+        loading(true);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .whereEqualTo(Constants.KEY_EMAIL,binding.inputEmail.getText().toString())
+                .whereEqualTo(Constants.KEY_PASSWORD,binding.inputPassword.getText().toString())
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful() && task.getResult() != null
+                     && task.getResult().getDocuments().size() > 0){
+                        DocumentSnapshot documentSnapshot = task.getResult().getDocuments().get(0);
+                        preferenceManager.putBoolean(Constants.KEY_IS_SIGNED_IN,true);
+                        preferenceManager.putString(Constants.KEY_USER_ID, documentSnapshot.getId());
+                        preferenceManager.putString(Constants.KEY_NAME,documentSnapshot.getString(Constants.KEY_NAME));
+                        preferenceManager.putString(Constants.KEY_IMAGE,documentSnapshot.getString(Constants.KEY_IMAGE));
+
+                        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                    }else {
+                        loading(false);
+                        showToast("Tài khoản và mật khẩu không đúng");
+                    }
+                });
+    }
+
+    private void loading(Boolean isLoading){
+        if (isLoading){
+            binding.buttonSignIn.setVisibility(View.INVISIBLE);
+            binding.progressBar.setVisibility(View.VISIBLE);
+        }else {
+            binding.progressBar.setVisibility(View.INVISIBLE);
+            binding.buttonSignIn.setVisibility(View.VISIBLE);
+        }
+    }
+    private void showToast(String message){
+        Toast.makeText(getApplicationContext(),message,Toast.LENGTH_SHORT).show();
+    }
+    private Boolean isValidSignInDetails(){
+        if (binding.inputEmail.getText().toString().trim().isEmpty()){
+            showToast("Chưa nhập email");
+            return false;
+        }else if (!Patterns.EMAIL_ADDRESS.matcher(binding.inputEmail.getText().toString()).matches()){
+            showToast("Nhập email hợp lệ");
+            return false;
+        }else if (binding.inputPassword.getText().toString().trim().isEmpty()){
+            showToast("Chưa nhập mật khẩu");
+            return false;
+        }else {
+            return true;
+        }
+    }
+
+}
